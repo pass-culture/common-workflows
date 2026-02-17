07e8e471af23837ec17aef8810b8436165e95fa2
Thibault Huillet <229985573+thuillet-pass@users.noreply.github.com>
2026-02-17 14:53:21 +0100

    (IC-880) actionlint workflow

---
+++ actions/actionlint-check/README.md
@@ -0,0 +1,13 @@
+# Setup safe-chain
+
+This action installs and runs [`actionlint`](https://github.com/pass-culture/actionlint).
+
+## Example
+
+```
+steps:
+    - name: setup
+    uses: actions/checkout@v6
+
+    - uses: pass-culture/common-workflows/actions/actionlint-check@<commit hash>
+```
---
+++ actions/actionlint-check/src/index.js
@@ -0,0 +1,57 @@
+import { getInput, addPath } from "@actions/core";
+import { exec } from "@actions/exec";
+import { mkdirP, cp } from "@actions/io";
+import { Readable } from "stream";
+import { finished } from "stream/promises";
+import { createWriteStream } from "fs";
+import { homedir } from "os";
+import { join } from "path";
+
+async function download(version, token) {
+  const assetsResponse = await fetch('https://api.github.com/repos/pass-culture/actionlint/releases', {
+    method: 'GET',
+    headers: {
+      "Accept": 'application/vnd.github+json',
+      "X-GitHub-Api-Version": "2022-11-28"
+    },
+  });
+  if (!assetsResponse.ok) {
+    throw new Error(`failed to get release with status code ${assetsResponse.status}: ${await assetsResponse.text()}`);
+  }
+  const assetsData = await assetsResponse.json();
+  const assets = assetsData.find(x => x.name === version)
+  const assetId = assets.assets.find(x => x.name === `actionlint_${version.replace(/^v/, '')}_linux_amd64.tar.gz`).id
+
+  const downloadResponse = await fetch(`https://api.github.com/repos/pass-culture/actionlint/releases/assets/${assetId}`, {
+    method: 'GET',
+    headers: {
+      "Accept": "application/octet-stream",
+      "X-GitHub-Api-Version": "2022-11-28"
+    },
+  });
+  if (!downloadResponse.ok) {
+    throw new Error(`failed to get release assets with status code ${downloadResponse.status}: ${await downloadResponse.text()}`);
+  }
+  const fileStream = createWriteStream("actionlint.tar.gz", { flags: 'wx' });
+  await finished(Readable.fromWeb(downloadResponse.body).pipe(fileStream));
+  await exec("tar", ["-xzvf", "actionlint.tar.gz"]);
+  return "actionlint"
+}
+
+async function install(downloadPath) {
+  const binPath = `${homedir}/bin`;
+  await mkdirP(binPath);
+  await cp(downloadPath, join(binPath, "actionlint"));
+  await exec("chmod", ["+x", `${binPath}/actionlint`]);
+  addPath(binPath);
+}
+
+async function run() {
+  const version = getInput("version")
+  const token = getInput("token")
+  const path = await download(version, token)
+  await install(path)
+  await exec("./actionlint", ["./.github/workflows/*.yml"]);
+}
+
+run();
